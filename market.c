d6f703b9230086730e9edcb8fdb064eb76dcb9b8
Pim van der Loos <pimvanderloos@gmail.com>
2014-09-11 14:47:45 +0200

    Start
    a’dsv

---
+++ market.c
@@ -0,0 +1,27 @@
+/* file : market.c */
+/* author : Lasse Vuursteen */
+/* date : Thursday 11 Sep 2014 */
+/* version : */
+
+/* Program Description:
+ *	
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+/* 10000 - pO*qO - pM*qM - pG*qG == 0 */
+
+int main(int argc, char*argv[]) {
+    int qO=0, qG=0, qM=0, pO, pG, pM, wallet=10000;
+	
+	printf("Enter the price of: Oranges, Grapefruit and Melon (in cents) \n");
+	scanf("%d, %d, %d", &pO, &pG, &pM);
+	
+	while(){
+		
+	}
+	
+    return 0;
+}
